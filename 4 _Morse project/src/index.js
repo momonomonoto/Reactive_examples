f0b2a27c0b0686253613945c6342b1fd314a9c1f
kirill <kr.shaidulin@prof-itgroup.ru>
2018-05-28 17:42:38 +0300

    Pause for morse

--- 4 _Morse project/src/index.js
+++ 4 _Morse project/src/index.js
@@ -4,37 +4,49 @@ import * as R from 'ramda';
 import morse from 'morse';
 
 const LINE_THRESHOLD_MS = 400;
+const LINE_SPACE = 800;
+
 const DOT = `.`;
 const LINE = `-`;
 
 const resultDate = getMilliseconds(new Date());
 const getTimeEventSpace = (event) => {
     return K.fromEvents(document, event)
-        .filter(event => event.keyCode === 32)
-        .map(() => getMilliseconds(new Date()) )
+        .map(() => new Date() )
 }
+
 
 const makeStore = (action$) => {
     return action$
         .merge(K.constant(``))
         .scan((state, fn) =>  fn(state))
-        .take(3)
 }
 console.log(resultDate,'new Date()');
-const timedSpaceUp$  = getTimeEventSpace(`keyup`);
 
-const timedSpaceDown$ = getTimeEventSpace(`keydown`);
+const getPauseObserable = (start$, end$) =>{
+    return K.combine([start$, end$], (b, a) => b - a)
+        .filter(ms => ms < LINE_SPACE)
+        .map(ms => ms > LINE_THRESHOLD_MS ? LINE : DOT)
+        .filter(Boolean);
+}
+const timedSpaceUp$  = getTimeEventSpace(`mousedown`);
 
-let dotOrLine$ = K.combine([timedSpaceDown$, timedSpaceUp$], (a, b) => a - b)
-    .filter(ms => ms > 0)
-    .map(ms => ms > LINE_THRESHOLD_MS ? LINE : DOT);
+const timedSpaceDown$ = getTimeEventSpace(`mouseup`);
+
+let timeout$ = timedSpaceUp$.flatMapConcat(_ => K.later(DOT)).map(_ => new Date())
+
+
+let pause$ = K.merge([timedSpaceDown$, timedSpaceUp$]).debounce(LINE_SPACE)
+
+
+let dotOrLine$ = getPauseObserable(timedSpaceDown$,timedSpaceUp$)
 
 const state$ = makeStore( K.merge([
-    dotOrLine$
-        .map(symbol => (state) => state + symbol)
+    dotOrLine$.map(symbol => (state) => state + symbol),
+    pause$.map(symbol => (state) => state + ' ')
 ]));
 
+state$.log();
 state$.onValue(state=>{
-    const resultSymbol = state.map(elemState=>morse.decode(elemState));
-    document.body.innerHTML = resultSymbol.join('');
+    document.body.innerHTML = state;
 })
